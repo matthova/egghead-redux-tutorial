fc9a1e79ce0d03d7556afab1ab341fc1702fc581
matthova <matt@hovalabs.com>
2018-03-04 15:20:07 -0800

    Turned TodoApp component into a stateless component

--- src/App.js
+++ src/App.js
@@ -16,39 +16,36 @@ const getVisibleTodos = (todos, filter) => {
 };
 
 let nextTodoId = 0;
-class TodoApp extends Component {
-  render() {
-    const { todos, store, visibilityFilter } = this.props;
+const TodoApp = ({ todos, store, visibilityFilter }) => {
+  const visibleTodos = getVisibleTodos(todos, visibilityFilter);
+  return (
+    <div>
+      <AddTodo
+        onAddClick={(text) => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            id: nextTodoId++,
+            text,
+          });
+        }}
+      />
+      <TodoList
+        todos={visibleTodos}
+        onTodoClick={id => store.dispatch({ type: 'TOGGLE_TODO', id })}
+      />
+      <VisibilityFilter
+        currentFilter={visibilityFilter}
+        onFilterClick={(filter) => {
+          store.dispatch({
+            type: 'SET_VISIBILITY_FILTER',
+            filter,
+          });
+        }}
+      />
+    </div>
+  );
+};
 
-    const visibleTodos = getVisibleTodos(todos, visibilityFilter);
-    return (
-      <div>
-        <AddTodo
-          onAddClick={(text) => {
-            store.dispatch({
-              type: 'ADD_TODO',
-              id: nextTodoId++,
-              text,
-            });
-          }}
-        />
-        <TodoList
-          todos={visibleTodos}
-          onTodoClick={id => store.dispatch({ type: 'TOGGLE_TODO', id })}
-        />
-        <VisibilityFilter
-          currentFilter={visibilityFilter}
-          onFilterClick={(filter) => {
-            store.dispatch({
-              type: 'SET_VISIBILITY_FILTER',
-              filter,
-            });
-          }}
-        />
-      </div>
-    );
-  }
-}
 const App = props => (
   <div>
     <TodoApp {...props} />
