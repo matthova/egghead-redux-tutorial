bfa748da9a4999e4108abe96860015d87c0c6309
matthova <matt@hovalabs.com>
2018-03-04 12:20:45 -0800

    Built a todo list reducer

--- src/App.js
+++ src/App.js
@@ -3,48 +3,88 @@ import { createStore } from 'redux';
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
-// Reducers
-const addCounter = list => [...list, 0];
-
-const removeCounter = (list, index) => [...list.slice(0, index), ...list.slice(index + 1)];
-
-const incrementCounter = (list, index) => [
-  ...list.slice(0, index),
-  list[index] + 1,
-  ...list.slice(index + 1),
-];
-
-// End reducers
-
-const testAddCounter = () => {
-  const listBefore = [];
-  const listAfter = [0];
-
-  deepFreeze(listBefore);
-
-  expect(addCounter(listBefore)).toEqual(listAfter);
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false,
+        },
+      ];
+    default:
+      return state;
+  }
 };
 
-const testRemoveCounter = () => {
-  const listBefore = [0, 10, 20];
-  const listAfter = [0, 20];
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux',
+  };
 
-  deepFreeze(listBefore);
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+  ];
 
-  expect(removeCounter(listBefore, 1)).toEqual(listAfter);
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
 };
 
-const testIncrementCounter = () => {
-  const listBefore = [0, 10, 20];
-  const listAfter = [0, 11, 20];
+testAddTodo();
 
-  deepFreeze(listBefore);
-  expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
-};
+// // Reducers
+// const addCounter = list => [...list, 0];
 
-testAddCounter();
-testRemoveCounter();
-testIncrementCounter();
+// const removeCounter = (list, index) => [...list.slice(0, index), ...list.slice(index + 1)];
+
+// const incrementCounter = (list, index) => [
+//   ...list.slice(0, index),
+//   list[index] + 1,
+//   ...list.slice(index + 1),
+// ];
+
+// // End reducers
+
+// const testAddCounter = () => {
+//   const listBefore = [];
+//   const listAfter = [0];
+
+//   deepFreeze(listBefore);
+
+//   expect(addCounter(listBefore)).toEqual(listAfter);
+// };
+
+// const testRemoveCounter = () => {
+//   const listBefore = [0, 10, 20];
+//   const listAfter = [0, 20];
+
+//   deepFreeze(listBefore);
+
+//   expect(removeCounter(listBefore, 1)).toEqual(listAfter);
+// };
+
+// const testIncrementCounter = () => {
+//   const listBefore = [0, 10, 20];
+//   const listAfter = [0, 11, 20];
+
+//   deepFreeze(listBefore);
+//   expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
+// };
+
+// testAddCounter();
+// testRemoveCounter();
+// testIncrementCounter();
 
 class App extends Component {
   constructor(props) {
