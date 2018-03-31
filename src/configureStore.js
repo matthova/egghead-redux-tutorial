1f2d0b799ac7e59ead75ed6c327fad276bf89e8e
matthova <matt@hovalabs.com>
2018-03-31 16:19:45 -0700

    Add logging helper function for debugging dispatches

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,9 +3,26 @@ import throttle from 'lodash/throttle';
 import todoApp from './reducers';
 import { loadState, saveState } from './localStorage';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(todoApp, persistedState);
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   store.subscribe(throttle(() => {
     saveState({
